26de2f03ae73fbca8cf771abbc986ce8e5b0fc44
lanaramiro <lana.ramiro@gmail.com>
2025-02-04 18:40:56 -0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('Yupiii')
+const searchInput = document.getElementById('search-input');
+const resultArtists = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlist')
+
+function requestApi(searchTerm) {
+    const url = 'http://localhost:3000/artists?name_like=${searchTerm}'
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
+
+function displayResults(result){
+    resultPlaylist.classList.add('hidden');
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-image');
+    
+    result.array.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtists.classList.remove('hidden'); 
+}
+
+document.addEventListener('input', function() {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === ''){
+        resultPlaylist.classList.add('hidden');
+        resultArtists.classList.remove('hidden');
+        return;
+    }
+
+    requestApi(searchTerm);
+});
